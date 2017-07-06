a30ed035d38a621eac8111ce89b3825efc15bab3
Tim Malone (Win) <tim.malone@chromatix.com.au>
2017-07-06 17:21:38 +1000

    Remove whitespace; fix some parent determining logic; sort refs alphabetically

--- scripts/massage.js
+++ scripts/massage.js
@@ -35,6 +35,8 @@ log.commits.forEach( function( commit ) {
 
     // Track the 'space' increases for this commit so we can adjust the parent's too.
     if ( 'undefined' === typeof spaces[ parent ] ) {
+      spaces[ parent ] = space;
+    } else {
       spaces[ parent ] = space - commit.space + 1;
     }
 
@@ -46,9 +48,9 @@ log.commits.forEach( function( commit ) {
   // Remove content we don't need from the refs.
   commit.refs = commit.refs.replace( /,|HEAD|->|tag:|origin\//g, ' ' ).replace( /\s+/g, ' ' ).trim();
 
-  // Make refs unique, in case we have both origin and local refs.
+  // Make refs unique, in case we have both origin and local refs, and then sort alphabetically.
   // HT: https://stackoverflow.com/a/14438954/1982136
-  commit.refs = [ ... new Set( commit.refs.split( ' ' ) ) ].join( ' ' );
+  commit.refs = [ ... new Set( commit.refs.split( ' ' ) ) ].sort().join( ' ' );
 
   // Increment time as we go.
   commit.time  = time;
